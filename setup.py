ac2a52d7e35e4acc7d3d4c11d3abf8102cdd3270
Pavel Pushkarev <paulus@yandex-team.ru>
2017-12-08 02:34:40 +0300

    Added debianization

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
+# pylint: disable=all
+
 from setuptools import setup, find_packages
 setup(
-    name="strace-analyser",
-    version="1",
-    author="Pavel Pushkarev",
-    author_email="media-admin@yandex-team.ru",
-    description=("The simple strace analisation routine"),
-    license="GPL",
-    url="https://github.yandex-team.ru/admins/strace-analyser",
-    packages=find_packages(),
-    scripts=[
-        'strace-analyser',
-        ],
-    install_requires=[
-        'yaconfig',
-        ],
+    name                = "strace-analyser",
+    version             = "1",
+    author              = "Pavel Pushkarev",
+    author_email        = "media-admin@yandex-team.ru",
+    description         = ("The simple strace analisation routine"),
+    license             = "GPL",
+    url                 = "https://github.yandex-team.ru/admins/strace-analyser",
+    packages            = find_packages(),
+    scripts             = ['strace-analyser'],
+    install_requires    = ['yaconfig'],
+    package_data = {
+        '': ['*.yaml'],
+    }
 )
--- strace_analyser/__init__.py
+++ strace_analyser/__init__.py
@@ -1,26 +1,4 @@
-"""
-This is a strace analyser module
+""" The strace_analyser module, see strace-analyser script for details """
 
-One should use it like this:
-
-strace -f -ttt -v -qq -T -s 1024 -p <pid> 2>&1 | strace-analyser
-"""
-
-import fileinput
-import yaconfig
-from StraceProcessor import StraceProcessor
-from SysCallAnalyser import SysCallAnalyser
-
-
-def main():
-    """ The main entry point """
-    conf = yaconfig.load_config('strace-analyser.yaml')
-
-    processor = StraceProcessor()
-    analyser = SysCallAnalyser(conf.syscalls.ignore)
-    for line in fileinput.input():
-        evt = processor.process(line)
-        analyser.process(evt)
-
-if __name__ == '__main__':
-    main()
+from .StraceProcessor import StraceProcessor
+from .SysCallAnalyser import SysCallAnalyser
